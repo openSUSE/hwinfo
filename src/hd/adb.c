aab02fb75cfb3e93e656b847daf7246de9d0ec28
Steffen Winterfeldt <wfeldt@opensuse.org>
2000-01-07 13:30:26 +0000

    - oops, forgot the adb files...

---
+++ src/hd/adb.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+
+#include "hd.h"
+#include "hd_int.h"
+#include "adb.h"
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * adb info
+ *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+
+#ifdef __PPC__
+
+void hd_scan_adb(hd_data_t *hd_data)
+{
+  int i;
+  unsigned u, adr = 0;
+  hd_t *hd;
+  str_list_t *sl;
+
+  if(!hd_probe_feature(hd_data, pr_adb)) return;
+
+  hd_data->module = mod_adb;
+
+  /* some clean-up */
+  remove_hd_entries(hd_data);
+
+  PROGRESS(1, 0, "get info");
+
+  for(sl = hd_data->klog; sl; sl = sl->next) {
+    if(sscanf(sl->str, "<4>ADB mouse at %u, %*[a-z ] %i", &u, &i) == 2 && u < 32) {
+      /* u: max 15 actually, but who cares... */
+      if(!(adr & (1 << u))) {
+        adr |= 1 << u;
+        hd = add_hd_entry(hd_data, __LINE__, 0);
+        hd->base_class = bc_mouse;
+        hd->sub_class = sc_mou_bus;
+        hd->bus = bus_adb;
+        hd->slot = u;
+        hd->func = i;
+        hd->unix_dev_name = new_str(DEV_ADBMOUSE);
+
+        hd->vend = name2eisa_id("PNP");
+        hd->dev = MAKE_ID(ID_EISA, 0x0f90);
+      }
+    }
+
+    if(sscanf(sl->str, "<4>ADB keyboard at %u, %*[a-z ] %i", &u, &i) == 2 && u < 32) {
+      /* u: max 15 actually, but who cares... */
+      if(!(adr & (1 << u))) {
+        adr |= 1 << u;
+        hd = add_hd_entry(hd_data, __LINE__, 0);
+        hd->base_class = bc_keyboard;
+        hd->sub_class = 0;
+        hd->bus = bus_adb;
+        hd->slot = u;
+        hd->func = i;
+//        hd->unix_dev_name = new_str(DEV_ADBMOUSE);
+
+        hd->vend = name2eisa_id("PNP");
+        hd->dev = MAKE_ID(ID_EISA, 0x0f91);
+      }
+    }
+  }
+}
+
+#endif	/* __PPC__ */
---
+++ src/hd/adb.h
@@ -0,0 +1,1 @@
+void hd_scan_adb(hd_data_t *hd_data);
