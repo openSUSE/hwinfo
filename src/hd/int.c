fcbaadc9b2fc10b852df148ee5eee56db096c1e6
Steffen Winterfeldt <wfeldt@opensuse.org>
2000-05-31 14:37:57 +0000

    - some files

---
+++ src/hd/int.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "hd.h"
+#include "hd_int.h"
+#include "int.h"
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * internal things
+ *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+
+#if defined(__i386__)
+static int set_bios_id(hd_data_t *hd_data, char *dev_name, int bios_id);
+static void int_bios(hd_data_t *hd_data);
+#endif
+
+void hd_scan_int(hd_data_t *hd_data)
+{
+  if(!hd_probe_feature(hd_data, pr_int)) return;
+
+  hd_data->module = mod_int;
+
+  /* some clean-up */
+  remove_hd_entries(hd_data);
+
+#if defined(__i386__)
+  PROGRESS(1, 0, "bios");
+  int_bios(hd_data);
+#endif
+
+}
+
+#if defined(__i386__)
+
+int set_bios_id(hd_data_t *hd_data, char *dev_name, int bios_id)
+{
+  int found = 0;
+  hd_t *hd;
+
+  for(hd = hd_data->hd; hd; hd = hd->next) {
+    if(
+      hd->base_class == bc_storage_device &&
+      hd->sub_class == sc_sdev_disk &&
+      hd->unix_dev_name &&
+      !strcmp(hd->unix_dev_name, dev_name)
+    ) {
+      str_printf(&hd->rom_id, 0, "0x%02x", bios_id);
+      found = 1;
+    }
+  }
+
+  return found;
+}
+
+void int_bios(hd_data_t *hd_data)
+{
+  hd_t *hd, *hd_boot;
+  int i, start, bios = 0x80;
+  int ide_1st;
+  char ide_name[] = "/dev/hda";
+  char scsi_name[] = "/dev/sda";
+  char *s;
+
+  hd_boot = hd_get_device_by_idx(hd_data, hd_boot_disk(hd_data, &i));
+
+  if(hd_boot) {
+    free_mem(hd_boot->rom_id);
+    hd_boot->rom_id = new_str("0x80");
+  }
+
+  if(!hd_boot || i != 1) return;
+
+  if(strstr(hd_boot->unix_dev_name, "/dev/sd") == hd_boot->unix_dev_name) {
+    ide_1st = 0;
+    start = hd_boot->unix_dev_name[sizeof "/dev/sd"] - 'a';
+  }
+  else if(strstr(hd_boot->unix_dev_name, "/dev/hd") == hd_boot->unix_dev_name) {
+    ide_1st = 1;
+    start = hd_boot->unix_dev_name[sizeof "/dev/hd"] - 'a';
+  }
+  else {
+    return;
+  }
+
+  for(hd = hd_data->hd; hd; hd = hd->next) {
+    if(
+      hd->base_class == bc_storage_device &&
+      hd->sub_class == sc_sdev_disk
+    ) {
+      hd->rom_id = free_mem(hd->rom_id);
+    }
+  }
+
+  s = ide_1st ? ide_name : scsi_name;
+
+  for(i = start; i < 26; i++) {
+    s[strlen(s) - 1] = 'a' + i;
+    bios += set_bios_id(hd_data, s, bios);
+  }
+
+  for(i = 0; i < start; i++) {
+    s[strlen(s) - 1] = 'a' + i;
+    bios += set_bios_id(hd_data, s, bios);
+  }
+
+  s = ide_1st ? scsi_name : ide_name;
+
+  for(i = 0; i < 26; i++) {
+    s[strlen(s) - 1] = 'a' + i;
+    bios += set_bios_id(hd_data, s, bios);
+  }
+
+}
+#endif	/* defined(__i386__) */
