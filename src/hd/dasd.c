3492711477674f72ac84d77a0759d37c43ccfa30
Steffen Winterfeldt <wfeldt@opensuse.org>
2000-09-05 14:09:53 +0000

    *** empty log message ***

---
+++ src/hd/dasd.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <linux/hdreg.h>
+
+#include "hd.h"
+#include "hd_int.h"
+#include "dasd.h"
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * s390 disk info
+ *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+
+#if defined(__s390__)
+
+void hd_scan_dasd(hd_data_t *hd_data)
+{
+  hd_t *hd;
+  char c;
+  unsigned u0, u2, u3;
+  str_list_t *sl, *sl0;
+  hd_res_t *res;
+  struct hd_geometry geo;
+  int i, fd;
+
+  if(!hd_probe_feature(hd_data, pr_dasd)) return;
+
+  hd_data->module = mod_dasd;
+
+  /* some clean-up */
+  remove_hd_entries(hd_data);
+
+  PROGRESS(1, 0, "read info");
+
+  sl0 = read_file(PROC_DASD "/devices", 0, 0);
+
+  i = 1;
+  for(sl = sl0; sl; sl = sl->next) {
+    if(sscanf(sl->str, "%x%*s at %*s is dasd%c:active at blocksize: %u, %u blocks", &u0, &c, &u2, &u3) == 4) {
+      hd = add_hd_entry(hd_data, __LINE__, 0);
+      hd->base_class = bc_storage_device;
+      hd->bus = bus_none;
+      hd->slot = (u0 >> 8) & 0xff;
+      hd->func = u0 & 0xff;
+
+      hd->sub_class = sc_sdev_disk;
+
+      hd->dev_name = new_str("S390 Disk");
+      str_printf(&hd->unix_dev_name, 0, "/dev/dasd%c", c);
+
+      res = add_res_entry(&hd->res, new_mem(sizeof *res));
+      res->size.type = res_size;
+      res->size.unit = size_unit_sectors;
+      res->size.val1 = u3;
+      res->size.val2 = u2;
+
+      fd = open(hd->unix_dev_name, O_RDONLY | O_NONBLOCK);
+      if(fd >= 0) {
+        PROGRESS(2, i++, "ioctl");
+        if(!ioctl(fd, HDIO_GETGEO, &geo)) {
+          ADD2LOG("dasd ioctl(geo) ok\n");
+          res = add_res_entry(&hd->res, new_mem(sizeof *res));
+          res->disk_geo.type = res_disk_geo;
+          res->disk_geo.cyls = geo.cylinders;
+          res->disk_geo.heads = geo.heads;
+          res->disk_geo.sectors = geo.sectors;
+          res->disk_geo.logical = 1;
+        }
+        close(fd);
+      }
+    }
+  }
+  free_str_list(sl0);
+}
+
+#endif	/* defined(__s390__) */
+
