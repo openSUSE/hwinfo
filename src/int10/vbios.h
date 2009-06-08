ee7934cc18c293871f91f103513bead32c96ba3f
Steffen Winterfeldt <wfeldt@opensuse.org>
2009-06-08 17:31:52 +0200

    - remove old x86 emulator files

--- src/int10/vbios.h
+++
@@ -1,6 +0,0 @@
-#include "hd.h"
-
-int InitInt10(hd_data_t *, int);
-int CallInt10(int *ax, int *bx, int *cx, unsigned char *buf, int len, int cpuemu);
-int CallInt13(int *ax, int *bx, int *cx, int *dx, unsigned char *buf, int len, int cpuemu);
-void FreeInt10(void);
