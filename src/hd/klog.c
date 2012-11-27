f7486bf20e1c792be8768c31fe00db24f39f2b93
Steffen Winterfeldt <wfeldt@opensuse.org>
2012-11-27 16:07:31 +0100

    - make kernel log parser aware of time stamps (bnc #544269, bnc #791200)

--- src/hd/klog.c
+++ src/hd/klog.c
@@ -17,6 +17,7 @@
 
 static int str_ok(str_list_t *sl);
 static int str_list_cmp(str_list_t *sl1, str_list_t *sl2);
+static void _read_klog(hd_data_t *hd_data);
 
 
 /*
@@ -41,10 +42,41 @@ int str_list_cmp(str_list_t *sl1, str_list_t *sl2)
   return 0;
 }
 
+
+/*
+ * Read kernel log info. Combine with /var/log/boot.msg.
+ * Remove time stamps.
+ */
+void read_klog(hd_data_t *hd_data)
+{
+  str_list_t *sl, **sl_new, *klog_raw;
+  char *str, *s;
+
+  _read_klog(hd_data);
+
+  hd_data->klog = free_str_list(hd_data->klog);
+  klog_raw = hd_data->klog;
+  hd_data->klog = NULL;
+
+  for(sl = klog_raw, sl_new = &hd_data->klog; sl; sl = sl->next, sl_new = &(*sl_new)->next) {
+    str = add_str_list(sl_new, sl->str)->str;
+    if(str[0] == '<' && str[1] && str[2] == '>' && str[3] == '[') {
+      s = str + 4;
+      while(*s && *s != ']') s++;
+      if(*s) s++;
+      if(*s) s++;	// skip space
+      strcpy(str + 3, s);
+    }
+  }
+
+  free_str_list(klog_raw);
+}
+
+
 /*
  * Read kernel log info. Combine with /var/log/boot.msg.
  */
-void read_klog(hd_data_t *hd_data)
+void _read_klog(hd_data_t *hd_data)
 {
   char buf[0x2000 + 1], *s;
   int i, j, len, n;
