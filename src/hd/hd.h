406113b60fc41f668254bf7f74ebcc3af39c3b52
Steffen Winterfeldt <wfeldt@opensuse.org>
2016-10-25 16:47:52 +0200

    implement nvdimm support (bsc #970111)
    Basically we have to create a 'storage controller' where we can attach the
    individual disk devices to.

--- src/hd/hd.h
+++ src/hd/hd.h
@@ -298,7 +298,7 @@ typedef enum bus_types {
   /** outside the range of the PCI values */
   bus_ps2 = 0x80, bus_serial, bus_parallel, bus_floppy, bus_scsi, bus_ide, bus_usb,
   bus_adb, bus_raid, bus_sbus, bus_i2o, bus_vio, bus_ccw, bus_iucv, bus_ps3_system_bus,
-  bus_virtio, bus_ibmebus, bus_gameport, bus_uisvirtpci, bus_mmc, bus_sdio
+  bus_virtio, bus_ibmebus, bus_gameport, bus_uisvirtpci, bus_mmc, bus_sdio, bus_nd
 } hd_bus_types_t;
 
 /** @} */
